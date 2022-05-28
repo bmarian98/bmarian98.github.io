4fb8e25f03a616a7104bbca4519a02b46b7c456a
YOUR-USERNAME <bodnarmarian873@yahoo.com>
2022-05-28 20:50:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -241,6 +241,68 @@ function updateTile(tile, num){
     }
 }
 
+document.addEventListener("touchstart", startTouch, false);
+document.addEventListener("touchmove", moveTouch, false);
+
+// Swipe Up / Down / Left / Right
+var initialX = null;
+var initialY = null;
+
+function startTouch(e) {
+    initialX = e.touches[0].clientX;
+    initialY = e.touches[0].clientY;
+};
+
+function moveTouch(e) {
+    if (initialX === null) {
+        return;
+    }
+
+    if (initialY === null) {
+        return;
+    }
+
+    var currentX = e.touches[0].clientX;
+    var currentY = e.touches[0].clientY;
+
+    var diffX = initialX - currentX;
+    var diffY = initialY - currentY;
+
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+        // sliding horizontally
+        if (diffX > 0) {
+            // swiped left
+            slideLeft();
+            console.log("swiped left");
+        } else {
+            // swiped right
+            slideRight();
+            console.log("swiped right");
+        }
+    } else {
+        // sliding vertically
+        if (diffY > 0) {
+            slideUp();
+            // swiped up
+            console.log("swiped up");
+        } else {
+            // swiped down
+            slideDown();
+            console.log("swiped down");
+        }
+    }
+
+    setTwo();
+    document.getElementById("score").innerText = score;
+
+    initialX = null;
+    initialY = null;
+
+    e.preventDefault();
+};
+
+
+
 
 document.addEventListener("keyup", (e) =>{
     if(e.code == "ArrowLeft"){
